5b36401f1389db352e1f9e656dec6ea55d68b392
jsukup <jesukup@gmail.com>
2016-10-25 10:51:46 -1000

    Finalized ggplots and added data to repo

--- reportanalysis.R
+++ reportanalysis.R
@@ -28,35 +28,37 @@ rownames(desc.by.state) <- NULL #Drop rownames
 il.counties <- st[st$State == 'IL',] #Subset counties by IL
 ca.counties <- st[st$State == 'CA',] #Subset counties by CA
 
+font_import()
 loadfonts(device = 'win') #Load fonts
 
 ##Histogram/density plot for TIS in IL
+png(filename = './Data/iltis.png', width = 950, height = 600)
 ggplot(il.counties, aes(Telehealth.Score.Raw.Adj)) +
     geom_histogram(aes(y = ..density..),
                    binwidth = 5,
                    color = 'Black', fill = '#2D4471') +
-    geom_density(alpha = .2, 
+    geom_density(alpha = .2,
                  fill = '#AAA838') +
-    geom_vline(aes(xintercept = median(Telehealth.Score.Raw.Adj, na.rm = T)),  
-               color = '#A7373D', 
-               linetype = 4, 
+    geom_vline(aes(xintercept = median(Telehealth.Score.Raw.Adj, na.rm = T)),
+               color = '#A7373D',
+               linetype = 4,
                size = 2) +
     labs(title = 'Distribution of Adjusted, Raw TIS: IL',
          y = 'Density',
          x = 'TIS') +
     annotate('text',
-             x = 58, 
-             y = .04, 
+             x = 58,
+             y = .04,
              label = 'Median IL TIS:\n 52.7',
              fontface = 'bold',
              family = 'Calibri') +
-    annotate('segment', 
-             x = 111, 
-             xend = 111, 
-             y = .01, 
-             yend = .005, 
-             colour = "#2D4471", 
-             size = 1, 
+    annotate('segment',
+             x = 111,
+             xend = 111,
+             y = .01,
+             yend = .005,
+             colour = "#2D4471",
+             size = 1,
              arrow = arrow()) +
     annotate('text',
              x = 111,
@@ -65,35 +67,37 @@ ggplot(il.counties, aes(Telehealth.Score.Raw.Adj)) +
              fontface = 'bold',
              family = 'Calibri') +
     theme(plot.title = element_text(family = 'Calibri', face = 'bold', size = 20),
-          axis.title = element_text(family = 'Calibri', face = 'bold', size = 12))
+          axis.title = element_text(family = 'Calibri', face = 'bold', size = 16))
+dev.off()
 
 ##Histogram/density plot for TIS in CA
+png(filename = './Data/catis.png', width = 950, height = 600)
 ggplot(ca.counties, aes(Telehealth.Score.Raw.Adj)) +
     geom_histogram(aes(y = ..density..),
                    binwidth = 5,
                    color = 'Black', fill = '#2D4471') +
-    geom_density(alpha = .2, 
+    geom_density(alpha = .2,
                  fill = '#AAA838') +
-    geom_vline(aes(xintercept = median(Telehealth.Score.Raw.Adj, na.rm = T)),  
-               color = '#A7373D', 
-               linetype = 4, 
+    geom_vline(aes(xintercept = median(Telehealth.Score.Raw.Adj, na.rm = T)),
+               color = '#A7373D',
+               linetype = 4,
                size = 2) +
     labs(title = 'Distribution of Adjusted, Raw TIS: CA',
          y = 'Density',
          x = 'TIS') +
     annotate('text',
-             x = 64, 
-             y = .04, 
+             x = 64,
+             y = .04,
              label = 'Median CA TIS:\n 59.2',
              fontface = 'bold',
              family = 'Calibri') +
-    annotate('segment', 
-             x = 99, 
-             xend = 99, 
-             y = .015, 
-             yend = .01, 
-             colour = "#2D4471", 
-             size = 1, 
+    annotate('segment',
+             x = 99,
+             xend = 99,
+             y = .015,
+             yend = .01,
+             colour = "#2D4471",
+             size = 1,
              arrow = arrow()) +
     annotate('text',
              x = 99,
@@ -102,5 +106,5 @@ ggplot(ca.counties, aes(Telehealth.Score.Raw.Adj)) +
              fontface = 'bold',
              family = 'Calibri') +
     theme(plot.title = element_text(family = 'Calibri', face = 'bold', size = 20),
-          axis.title = element_text(family = 'Calibri', face = 'bold', size = 12))    
-
+          axis.title = element_text(family = 'Calibri', face = 'bold', size = 16))
+dev.off()
