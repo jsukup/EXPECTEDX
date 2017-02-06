cb9a15b473982392a40a39d9cdfe7a6b46daac17
jsukup <jesukup@gmail.com>
2017-02-06 13:35:09 -1000

    Added updates suggested by Chrisss on StackOverflow

--- EmotionAPI/emotionapi.R
+++ EmotionAPI/emotionapi.R
@@ -1,10 +1,22 @@
 library(httr)
+library(magrittr)
+library(purrr)
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 
 apiUrl <- 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognizeinvideo?outputStyle=perFrame'
 
 key <- '935271b2f2ae41e7b4298ce3c7216ec1'
 
-urlVideo <- 'https://drive.google.com/file/d/0B5EuiMJDiDhWdWZSUGxvV3lJM0U/view?usp=sharing'
+orig_url <- 'https://1drv.ms/v/s!As_lzte5x5PDaPyuv0F3AN91nHo'; 
+
+urlVideo <- RCurl::base64(orig_url) %>% 
+    gsub('\\+', '\\-', .) %>% 
+    gsub('\\/', '\\_', .) %>% 
+    sub('\\=$', '', .) %>% 
+    paste0('https://api.onedrive.com/v1.0/shares/u!', ., '/root/content')
 
 mybody <- list(url = urlVideo)
 
@@ -16,30 +28,31 @@ faceEMO <- httr::POST(
     encode = 'json'
 )
 
-operationLocation <- httr::headers(faceEMO)[["operation-location"]]
+operationLocation <- httr::headers(faceEMO)[['operation-location']]
 
 while(TRUE){
     ret <- httr::GET(operationLocation,
                      httr::add_headers(.headers = c('Ocp-Apim-Subscription-Key' = key)))
     con <- httr::content(ret)
     if(is.null(con$status)){
-        warning("Connection Error, retry after 1 minute")
+        warning('Connection Error, retry after 1 minute')
         Sys.sleep(60)
-    } else if (con$status == "Running" | con$status == "Uploading"){
-        cat(paste0("status ", con$status, "\n"))
-        cat(paste0("progress ", con$progress, "\n"))
+    } else if (con$status == 'Running' | con$status == 'Uploading'){
+        cat(paste0('status ', con$status, '\n'))
+        cat(paste0('progress ', con$progress, '\n'))
         Sys.sleep(60)
     } else {
-        cat(paste0("status ", con$status, "\n"))
+        cat(paste0('status ', con$status, '\n'))
         break()
     }
 }
+
 data <- (con$processingResult %>% jsonlite::fromJSON())$fragments
-# data$events is list of events that has data.frame column,
-# so it has to be flatten in this loop
 data$events <- purrr::map(data$events, function(events){
     events %>% purrr::map(function(event){
         jsonlite::flatten(event)
-    }) %>% bind_rows()
+    }) %>% dplyr::bind_rows()
 })
-data
+
+data <- unnest(data,events)
+data$frame.id <- as.numeric(rownames(data))
