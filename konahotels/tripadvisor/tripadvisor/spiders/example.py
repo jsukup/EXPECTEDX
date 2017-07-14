14b154cd0c145b5503aa0460014e19f2e9aa9ab6
jsukup <jesukup@gmail.com>
2017-07-13 16:09:43 -1000

    7/13 updates

--- konahotels/tripadvisor/tripadvisor/spiders/example.py
+++ konahotels/tripadvisor/tripadvisor/spiders/example.py
@@ -8,8 +8,18 @@ class TrSpider(scrapy.Spider):
             'https://www.tripadvisor.com/Hotels-g29217-Island_of_Hawaii_Hawaii-Hotels.html'
             ]
 
+    def parse(self, response):
+        for href in response.xpath('//div[@class="listing_title"]/a/@href'):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_hotel)
+
+        next_page = response.xpath('//div[@class="unified pagination standard_pagination"]/child::*[2][self::a]/@href')
+        if next_page:
+            url = response.urljoin(next_page[0].extract())
+            yield scrapy.Request(url, self.parse)
+
     def parse_hotel(self, response):
-        for href in response.xpath('//div[@class="quote"]/a/@href'):
+        for href in response.xpath('//div[starts-with(@class,"quote")]/a/@href'):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url, callback=self.parse_review)
 
@@ -18,13 +28,13 @@ class TrSpider(scrapy.Spider):
             url = response.urljoin(next_page[0].extract())
             yield scrapy.Request(url, self.parse_hotel)
 
-
     def parse_review(self, response):
         item = TripadvisorItem()
-        item['headline'] = [response.xpath('//div[@class="quote"]/text()').extract()[0][1:-1]]
-        item['review'] = response.xpath('translate(//div[@class="entry"]/p,"\n"," ")').extract()
-
-
+        item['headline'] = [response.xpath('translate(//div[@class="quote"]/text(),"!"," ")').extract()[0][1:-1]]
+        item['review'] = response.xpath('translate(//div[@class="entry"]/p,"\n"," ")').extract()[0]
+        item['bubbles'] = [r.split('_')[-1] for r in response.xpath("//div[contains(@class, 'reviewItemInline')]//span[contains(@class, 'ui_bubble_rating')]/@class").extract()][0]
+        item['date'] = response.xpath('//span[@class="ratingDate relativeDate"]/@title').extract()
+        item['hotel'] = response.xpath('normalize-space(//span[@class="altHeadInline"]/a/text())').extract()[0]
 
 
 
@@ -37,4 +47,5 @@ class TrSpider(scrapy.Spider):
 #        item['date'] = response.xpath('//span[@class="ratingDate relativeDate"]/@title').extract()
 #        item['hotel'] = response.xpath('normalize-space(//div[@id="taplc_location_detail_header_hotels_0"]/h1/text())').extract()
 #        return item
+
 
